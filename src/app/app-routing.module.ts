2b0c0175032ca9562ad0384a4898276c2a142346
shahraj <sh880821@dal.ca>
2023-06-06 03:44:48 -0300

    changes in footer mobile and added new components

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,15 +1,21 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AppComponent } from './app.component';
+import { AboutComponent } from './pages/about/about.component';
 
 const routes: Routes = [
-  {path: "about", component: AppComponent},
-  {path: "projects", component: AppComponent},
-  {path: "contact", component: AppComponent}
+  {
+    path: '',
+    redirectTo: 'about',
+    pathMatch: 'full',
+  },
+  { path: 'about', component: AboutComponent },
+  { path: 'projects', component: AppComponent },
+  { path: 'contact', component: AppComponent },
 ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,12 +6,16 @@ import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NavbarComponent } from './components/navbar/navbar.component';
 import { FooterComponent } from './components/footer/footer.component';
+import { InfocardComponent } from './components/infocard/infocard.component';
+import { AboutComponent } from './pages/about/about.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavbarComponent,
-    FooterComponent
+    FooterComponent,
+    InfocardComponent,
+    AboutComponent
   ],
   imports: [
     BrowserModule,
